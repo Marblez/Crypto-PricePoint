e9a9a10299b9a9d15f0271f64a511e20dee7007d
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-09-19 00:35:52 -0500

    Script

--- CryptoUpdate.js
+++ CryptoUpdate.js
@@ -1,16 +1,96 @@
 // Required Configurations: PhantomJS Library, Configured PhantomJS PATH variable, Windows CMD Batch
 
-
 var page = require('webpage').create();
+  //BITCOIN PRICES AND CHANGES
+  page.open('https://coinmarketcap.com/currencies/bitcoin/#markets', function(status) {
+    if (status === "success") {
+      console.log("Success");
 
-page.open('https://coinmarketcap.com/', function(status) {
-  if (status === "success") {
-    page.render('renderfirst.pdf', {
-      format: 'pdf',
-      quality: '100'
+      var BTC = page.evaluate(function(){
+    //  var price = document.getElementById("quote_price");
+      var price1 = document.getElementById("quote_price").innerHTML;
+      return price1;
     });
-    
-    phantom.exit();
+    var content = BTC;
+    console.log(content);
 
-  }
-});
+    var BTC2 = page.evaluate(function(){
+      try{
+    // Try to see if the change is negative first
+      var elementlist = document.getElementsByClassName("text-large  negative_change");
+      var pricechange = elementlist[0].innerHTML;
+      return pricechange;
+    }
+    catch(ex){
+      var elementlist2 = document.getElementsByClassName("text-large  positive_change");
+      var pricechange2 = elementlist2[0].innerHTML;
+      return pricechange2;
+    }
+    });
+    var content2 = BTC2;
+    console.log(content2);
+
+    }
+  });
+
+  page.open('https://coinmarketcap.com/currencies/ethereum/#markets', function(status) {
+    if (status === "success") {
+      console.log("Success");
+
+      var ETH = page.evaluate(function(){
+    //  var price = document.getElementById("quote_price");
+      var price1 = document.getElementById("quote_price").innerHTML;
+      return price1;
+    });
+    var content = ETH;
+    console.log(content);
+
+    var ETH2 = page.evaluate(function(){
+      try{
+    // Try to see if the change is negative first
+      var elementlist = document.getElementsByClassName("text-large  negative_change");
+      var pricechange = elementlist[0].innerHTML;
+      return pricechange;
+    }
+    catch(ex){
+      var elementlist2 = document.getElementsByClassName("text-large  positive_change");
+      var pricechange2 = elementlist2[0].innerHTML;
+      return pricechange2;
+    }
+    });
+    var content2 = ETH2;
+    console.log(content2);
+    }
+  });
+
+
+  page.open('https://coinmarketcap.com/currencies/litecoin/#markets', function(status) {
+    if (status === "success") {
+      console.log("Success");
+
+      var LTC = page.evaluate(function(){
+    //  var price = document.getElementById("quote_price");
+      var price1 = document.getElementById("quote_price").innerHTML;
+      return price1;
+    });
+    var content = LTC;
+    console.log(content);
+
+    var LTC2 = page.evaluate(function(){
+      try{
+    // Try to see if the change is negative first
+      var elementlist = document.getElementsByClassName("text-large  negative_change");
+      var pricechange = elementlist[0].innerHTML;
+      return pricechange;
+    }
+    catch(ex){
+      var elementlist2 = document.getElementsByClassName("text-large  positive_change");
+      var pricechange2 = elementlist2[0].innerHTML;
+      return pricechange2;
+    }
+    });
+    var content2 = LTC2;
+    console.log(content2);
+      phantom.exit();
+    }
+  });
