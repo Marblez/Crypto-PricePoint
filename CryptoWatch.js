9f1c7950d0030e42173759b36a4766e6b892ed48
Marblez <MatthewWang2020@u.northwestern.edu>
2017-12-15 00:36:06 -0600

    String Parsing

--- CryptoWatch.js
+++ CryptoWatch.js
@@ -32,12 +32,16 @@ function checkLitecoin(){
 
 }
 
-function sendText(numbers,coin,message,value){
+function percentageText(numbers,coin,value,change,time){
   // Twilio API 
   for (i = 0; i < numbers.length; i++) {
   	var currnumber = numbers[i];
+  	var str = " had a ";
+  	var space = " ";
+  	var timemessage = " in the past ";
+  	var finalmessage = coin.concat(str,value,space,change,timemessage,time);
     client.messages.create({
-    body: 'Values hit threshhold',
+    body: finalmessage,
     to: currnumber,  
     from: twilioNumber // From a valid Twilio number
 })
@@ -46,6 +50,21 @@ function sendText(numbers,coin,message,value){
   
 }
 
+function thresholdText(numbers,coin,value){
+  // Twilio API 
+  for (i = 0; i < numbers.length; i++) {
+  	var currnumber = numbers[i];
+  	var str = " just hit a threshold of ";
+  	var finalmessage = coin.concat(str,value);
+    client.messages.create({
+    body: finalmessage,
+    to: currnumber,  
+    from: twilioNumber // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
+} 
+  
+}
 // Potential additional features:
 // (1) Google Trends API for analytics
 // (2) Saving datasets onto database
