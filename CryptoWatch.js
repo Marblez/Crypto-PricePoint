a60080b49594123559ea0c18d51c6f92e64861ab
Marblez <MatthewWang2020@u.northwestern.edu>
2017-12-15 00:26:52 -0600

    Twilio API

--- CryptoWatch.js
+++ CryptoWatch.js
@@ -1,7 +1,13 @@
 'use strict';
-// Required Configurations: NodeJS, Google Trends API
+// Required Configurations: NodeJS, Google Trends API, Twilio
 // BlockTrail API
 // Past Datasets: https://www.coindesk.com/price/
+var accountSid = ''; 
+var authToken = ''; 
+var twilioNumber = '';
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+var numbers = ['000000','0000001','00000002'] ;
 
 setInterval(function(){
   // Functions for checking prices of cryptocurrencies
@@ -26,8 +32,18 @@ function checkLitecoin(){
 
 }
 
-function sendText(number,coin,message,value){
-  // Twilio API POST
+function sendText(numbers,coin,message,value){
+  // Twilio API 
+  for (i = 0; i < numbers.length; i++) {
+  	var currnumber = numbers[i];
+    client.messages.create({
+    body: 'Values hit threshhold',
+    to: currnumber,  
+    from: twilioNumber // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
+} 
+  
 }
 
 // Potential additional features:
