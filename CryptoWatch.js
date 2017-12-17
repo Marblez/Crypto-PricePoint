d5f6fce10f1ac1955410c5c692ed7185b70e0953
Marblez <MatthewWang2020@u.northwestern.edu>
2017-12-17 00:56:12 -0600

    Text Protocols

--- CryptoWatch.js
+++ CryptoWatch.js
@@ -12,14 +12,32 @@ var twilioNumber = '+12176567597';
 //var coinCap = require('./coinMarketCap');
 
 setInterval(function(){
-  thresholdText('Bitcoin','$20000');
+  //percentChange('Bitcoin','increase','40',24);
+  //thresholdText('Ethereum',900);
 
 }, 5000);
 
 function thresholdText(coin,value){
   for(let i = 0; i<numbers.length; i++){
-    var str1 = "has reached the price of "
+    var str1 = " has reached the price of "
     var message = coin.concat(str1,value);
+    client.messages.create({
+        body: message,
+        to: numbers[i],  // Text this number
+        from: twilioNumber // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+  }
+}
+
+function percentChange(coin,change,percent,time){
+  for(let i = 0; i<numbers.length; i++){
+    var str1 = " has "
+    var space = " by ";
+    var str2 = "%";
+    var str3 = " in the last "
+    var str4 = " hours."
+    var message = coin.concat(str1,change,space,percent,str2,str3,time,str4);
     client.messages.create({
         body: message,
         to: numbers[i],  // Text this number
