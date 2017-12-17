53ded02fb0054332d0495b2740ede557174b7871
Tim Chang <tinghaur@umail.ucsb.edu>
2017-12-17 00:04:38 -0800

    Coinmarketcap works but not GDAX's api

--- coinCap.js
+++ coinCap.js
@@ -1,10 +1,21 @@
 'use strict';
 //This is the module that houses the function calls
 // from coinmarketcap
-let url = 'https://api.coinmarketcap.com/v1/ticker/?limit=15'
-function retrieveJSON(){
-	//has something to do with an XMLhttprequest????
-}
+
+var https = require('https');
+
+var url = 'https://api.gdax.com/products/ETH-USD/';
+//var url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
+var request = require("request");
+var jsonObject;
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+
+  }
+  	 jsonObject = JSON.parse(body);
+     console.log(jsonObject);
+});
+/*
 module.exports = {
 	function bitcoin(){
 		retrieveJSON(); // should do something to parse the JSON
@@ -23,4 +34,4 @@ module.exports = {
  		//same thing as above
  		//is in 4th position, 3rd position is ripple i believe
  	}
-}
+}*/
