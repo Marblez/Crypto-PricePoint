1932d11d00f844cbb480a9ae421a7c4d40842e54
Marblez <MatthewWang2020@u.northwestern.edu>
2017-12-18 22:17:27 -0600

    CryptoWatch Prototype

--- CryptoWatch.js
+++ CryptoWatch.js
@@ -1,12 +1,17 @@
 'use strict';
-// Required Configurations: NodeJS, Google Trends API, Twilio
+// Required Configurations: NodeJS, GDAX API, Twilio API
 // BlockTrail API
 // Past Datasets: https://www.coindesk.com/price/
 // SET THRESHOLDS
 var eth = 800;
 var btc = 20000;
 var ltc = 400;
-
+var jobj;
+var ethprice;
+var btcprice;
+var ltcprice;
+var https = require('https');
+var request = require("request");
 var accountSid = 'ACf6e2c57a49f3a5266e44e4e292c94b5a';
 var authToken = '069cf667c68c5e3624ba7f6a1ee68564';
 var twilio = require('twilio');
@@ -19,8 +24,10 @@ var twilioNumber = '+12176567597';
 setInterval(function(){
   //percentChange('Bitcoin','increase','40',24);
   //thresholdText('Ethereum',900);
+  checkCoins();
 
-}, 5000);
+
+}, 3000);
 
 function checkPercentage(percentage){
   if(percentage>10){
@@ -31,8 +38,29 @@ function checkPercentage(percentage){
   }
 }
 
-function checkThreshold(threshold,price){
-  if(threshold<price){
+function checkETHThreshold(price){
+  if(eth<price){
+    eth = eth + 100;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function checkBTCThreshold(price){
+  if(btc<price){
+    btc = btc + 1000;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function checkLTCThreshold(price){
+  if(ltc<price){
+    ltc = ltc + 100;
     return true;
   }
   else{
@@ -80,6 +108,103 @@ function runtest(){
     .then((message) => console.log(message.sid));
   }
 }
+
+function checkCoins(){
+var linketh = {
+  url: 'https://api.gdax.com/products/ETH-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+var linkbtc = {
+  url: 'https://api.gdax.com/products/BTC-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+var linkltc = {
+  url: 'https://api.gdax.com/products/LTC-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+request(linketh, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   ethprice = jobj.price
+});
+request(linkbtc, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   btcprice = jobj.price
+});
+request(linkltc, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   ltcprice = jobj.price
+});
+setTimeout(function(){
+//console.log(ltcprice);
+//console.log(btcprice);
+//console.log(ethprice);
+if(checkETHThreshold(ethprice)){
+  thresholdText('Ethereum',ethprice);
+}
+if(checkBTCThreshold(btcprice)){
+  thresholdText('Bitcoin',ethprice);
+}
+if(checkLTCThreshold(ltcprice)){
+  thresholdText('Litecoin',ethprice);
+}
+}, 1000);
+
+}
+/*
+var https = require('https');
+var request = require("request");
+var jobj;
+var ethprice;
+var btcprice;
+var ltcprice;
+var linketh = {
+  url: 'https://api.gdax.com/products/ETH-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+var linkbtc = {
+  url: 'https://api.gdax.com/products/BTC-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+var linkltc = {
+  url: 'https://api.gdax.com/products/LTC-USD/ticker',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+request(linketh, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   ethprice = jobj.price
+});
+request(linkbtc, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   btcprice = jobj.price
+});
+request(linkltc, function (error, response, body) {
+     jobj = JSON.parse(body);
+   //console.log(jobj.price);
+   ltcprice = jobj.price
+});
+setTimeout(function(){
+console.log(ltcprice);
+console.log(btcprice);
+console.log(ethprice);
+}, 1000);
+*/
 // Potential additional features:
 // (1) Google Trends API for analytics
 // (2) Saving datasets onto database
